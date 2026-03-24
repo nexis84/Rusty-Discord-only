135ddb974619743b7ea55293006d0eb110e00fea
Adam <nexisenb@googlemail.com>
2026-03-24 04:34:25 +0000

    Keep only website files

--- irc_fallback_reference.py
+++
@@ -1,5 +0,0 @@
-# This is a reference copy of the working IRC fallback implementation
-# We'll examine this to understand how message sending works in the working bot
-
-# Could you please copy the contents of irc_fallback.py here?
-# Or share the key parts of how your working bot sends messages?
